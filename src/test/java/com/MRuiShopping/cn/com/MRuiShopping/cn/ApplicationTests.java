e6cc299778926feb289753ba424df23cc02650ea
894923712 <894923712@qq.com>
2019-04-10 11:34:57 +0800

    添加创建二维码工具类

--- src/test/java/com/MRuiShopping/cn/com/MRuiShopping/cn/ApplicationTests.java
+++ src/test/java/com/MRuiShopping/cn/com/MRuiShopping/cn/ApplicationTests.java
@@ -1,16 +1,22 @@
 package com.MRuiShopping.cn.com.MRuiShopping.cn;
 
+import Utils.CreatQRCode;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
 
+
 	@Test
 	public void contextLoads() {
+		CreatQRCode.creatQrcode("你好，我是羅明","D:/qrcode.png");
 	}
 
 }
